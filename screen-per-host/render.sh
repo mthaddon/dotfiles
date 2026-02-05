b8fc4a4391316d34f8410e2b41bbe2994100aa29
Tom Haddon <tom@haddons.net>
2026-02-05 14:29:02 +0100

    Ensure we have the screen directory

--- screen-per-host/render.sh
+++ screen-per-host/render.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Ensure the directory we're going to render into exists.
+mkdir -p ${HOME}/.dotfiles/screen
+
+# Copy our base screenrc file.
 cp ${HOME}/.dotfiles/screen-per-host/dot-screenrc ${HOME}/.dotfiles/screen/dot-screenrc
+
+# If we have a per-host windows file, append it to the screenrc file.
 if [ -f "${HOME}/.dotfiles/screen-per-host/${HOSTNAME}-windows" ]; then
 	cat "${HOME}/.dotfiles/screen-per-host/${HOSTNAME}-windows" >> ~/.dotfiles/screen/dot-screenrc
 fi
